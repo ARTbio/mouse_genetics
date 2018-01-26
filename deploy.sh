26b6707e8d2839872a3508ece280acbae3f34272
drosofff <drosofff@gmail.com>
2018-01-27 00:33:26 +0100

    fix name of the repo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ mkdir mkdocs_build
 cd mkdocs_build
 # Initialize gh-pages checkout
 DATE=`date`
-git clone https://github.com/ARTbio/Run-Galaxy.git
-cd Run-Galaxy
+git clone https://github.com/ARTbio/mouse_genetics.git
+cd mouse_genetics
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 mkdocs gh-deploy --clean -m "gh-deployed by travis $DATE"
